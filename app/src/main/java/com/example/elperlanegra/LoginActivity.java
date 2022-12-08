b5ae51d04d7136d86c42f577b74c21a2851e54b4
Geraldine <gbdrouet@itb.edu.ec>
2022-12-08 11:50:46 -0500

    diseño de registro y login listo

--- app/src/main/java/com/example/elperlanegra/LoginActivity.java
+++ app/src/main/java/com/example/elperlanegra/LoginActivity.java
@@ -2,13 +2,41 @@ package com.example.elperlanegra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button login;
+    EditText email, pass;
+    TextView registro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = (Button) findViewById(R.id.login_btn);
+        email = (EditText) findViewById(R.id.email_log);
+        pass = (EditText) findViewById(R.id.pass_log);
+        registro = (TextView) findViewById(R.id.reg_link);
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegistroActivity.class));
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/elperlanegra/RegistroActivity.java
+++ app/src/main/java/com/example/elperlanegra/RegistroActivity.java
@@ -2,13 +2,43 @@ package com.example.elperlanegra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegistroActivity extends AppCompatActivity {
+
+    Button registro;
+    EditText nombre, direccion, email, password;
+    TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        registro = (Button) findViewById(R.id.reg_btn);
+        nombre = (EditText) findViewById(R.id.name_reg);
+        direccion = (EditText) findViewById(R.id.direccion_reg);
+        email = (EditText) findViewById(R.id.email_reg);
+        password = (EditText) findViewById(R.id.pass_reg);
+        login = (TextView) findViewById(R.id.login_link);
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistroActivity.this,LoginActivity.class));
+            }
+        });
     }
 }
